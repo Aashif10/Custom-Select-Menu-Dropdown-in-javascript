4dd09fdcd7e4fe4ec4e217db0fda90962ca416e7
Aashif <aashif3225@gmail.com>
2024-05-13 00:47:22 +0530

    DropDown Menu

---
+++ script.js
@@ -0,0 +1,12 @@
+function show(anything) {
+    document.getElementById("input").value = anything;   
+}
+
+let down = document.getElementById("dropdown");
+down.onclick =function() {
+    down.classList.toggle("active")
+}
+
+
+
+
